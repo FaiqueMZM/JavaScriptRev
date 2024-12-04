db841dd8bfb82412db0ce3335888c8057e38c320
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-12-03 19:17:33 -0800

    added objects and object methods

---
+++ ObjectsAndObjectMethods.js
@@ -0,0 +1,20 @@
+// objects and object methods
+console.log("Objects and object methods");
+
+// creating and accessing objetcs
+console.log("Creating and accessing objects");
+
+let person = { name: "John", age: 30, sex: "male" };
+console.log("Person object : " + person);
+console.log("Name : " + person.name);
+console.log("Age : " + person.age);
+
+// addind, updating and deleting properties
+console.log("Adding, updating and deleting properties");
+
+person.city = "New York";
+console.log("City : " + person.city);
+console.log("Updated person object : " + person);
+
+person.age = 31;
+console.log("Age : " + person.age);
