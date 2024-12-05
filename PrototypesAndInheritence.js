ac9e183be6af11a8cb589ec9c94339df169ef8f9
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-12-04 23:16:40 -0800

    updated protypes and inheritence

--- PrototypesAndInheritence.js
+++ PrototypesAndInheritence.js
@@ -49,3 +49,62 @@ let charles = new Person("Charles");
 
 max.greet(); // "Hello, my name is Max"
 charles.greet(); // "Hello, my name is Charles"
+
+// es6 classes
+console.log("ES6 classes");
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a noice`);
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks`);
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log(`${this.name} meows`);
+  }
+}
+
+let animal1 = new Animal("Animal");
+let dog1 = new Dog("Buddy");
+let cat1 = new Cat("Donut");
+
+animal1.speak(); // Animal makes a noice
+dog1.speak(); // Buddy barks
+cat1.speak(); // Donut meows
+
+// checking prototypes
+console.log("Checking prototypes");
+
+let obj = {};
+console.log(Object.getPrototypeOf(obj));
+
+console.log(Object.getPrototypeOf(animal1));
+console.log(Object.getPrototypeOf(dog1));
+console.log(Object.getPrototypeOf(cat1));
+
+// overriding prototypes
+console.log("Overriding prototypes");
+
+let animal2 = {
+  speak() {
+    console.log("Animal makes a sound");
+  },
+};
+
+let dog2 = Object.create(animal2);
+dog2.speak = function () {
+  console.log("Dog barks");
+};
+
+animal2.speak(); // Animal makes a sound
+dog2.speak(); // Dog barks
