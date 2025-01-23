ddd523d520ffeb4975f28e6b0472a97e88cae608
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-23 20:49:52 +0530

    added merge sorted arrays

---
+++ DataStructureAndAlgorithm/Arrays/mergeSortedArrays.js
@@ -0,0 +1,21 @@
+// merge two sorted arrays
+
+function mergeSortedArrays(arr1, arr2) {
+    let result = [];
+  let i = 0, j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i++]);
+    } else {
+      result.push(arr2[j++]);
+    }
+  }
+
+  while (i < arr1.length) result.push(arr1[i++]);
+  while (j < arr2.length) result.push(arr2[j++]);
+
+  return result;
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
