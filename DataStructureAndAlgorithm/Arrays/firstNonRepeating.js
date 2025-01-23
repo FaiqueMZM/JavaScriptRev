ba998d900e06b2dd32b2798cc02be4008bf07d53
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-23 20:44:30 +0530

    added first non repeating element

---
+++ DataStructureAndAlgorithm/Arrays/firstNonRepeating.js
@@ -0,0 +1,19 @@
+// first non reapeating element in an array
+
+function firstNonReapeatingElement(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                count++;
+            }
+        }
+        if (count === 1) {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+console.log(firstNonReapeatingElement([9, 2, 3, 2, 3, 4, 5])); // Output: 9
+console.log(firstNonReapeatingElement([3 , 3, 1, 2, 3, 4, 5])); // Output: 1
