61a29965f3be3c07a5ab7743e8db77246bfd2ac6
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-25 09:15:14 +0530

    added new folder objects

---
+++ DataStructureAndAlgorithm/Objects/countOccurence.js
@@ -0,0 +1,12 @@
+// count occurence of each element in an array using an object
+
+function countOccurence(arr) {
+    let count = {};
+
+    for (let item of arr) {
+        count[item] = (count[item] || 0) + 1;
+    }
+    return count;
+}
+
+console.log(countOccurence([1, 2, 3, 2, 3, 4, 5])); // Output: { 1: 1, 2: 2, 3: 2, 4: 1, 5: 1 }
