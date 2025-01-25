e4b1b082799b9e75f985b17da4597483d21f0928
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-25 09:37:43 +0530

    updated merge two objects

--- DataStructureAndAlgorithm/Objects/mergeTwoObjects.js
+++ DataStructureAndAlgorithm/Objects/mergeTwoObjects.js
@@ -1,7 +1,7 @@
 // merge two objects, duplicate key's values will be summed
 
 function mergeTwoObjects(obj1, obj2) {
-    let merged = {...obj1 };
+    let merged = { ...obj1 };
 
     for (let key in obj2) {
         merged[key] = (merged[key] || 0) + obj2[key];
@@ -11,3 +11,21 @@ function mergeTwoObjects(obj1, obj2) {
 }
 
 console.log(mergeTwoObjects({ a: 1, b: 2 }, { b: 3, c: 4 })); // Output: { a: 1, b: 5, c: 4 }
+
+// alternate implementation
+
+function mergeTwoObjects(obj1, obj2) {
+    const merged = { ...obj1 }; 
+
+    for (const key in obj2) {
+        if (key in merged) {
+            merged[key] += obj2[key];
+        } else {
+            merged[key] = obj2[key]; 
+        }
+    }
+
+    return merged;
+}
+
+console.log(mergeTwoObjects({ a: 5, b: 1 }, { b: 2, c: 6 })); // Output: { a: 5, b: 3, c: 6 }
