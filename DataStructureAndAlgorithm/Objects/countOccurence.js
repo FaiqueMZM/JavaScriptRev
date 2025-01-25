0803d9cb9ad208d8f7e7f770c75539ac3dc287d7
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-25 09:18:01 +0530

    updated count occurence

--- DataStructureAndAlgorithm/Objects/countOccurence.js
+++ DataStructureAndAlgorithm/Objects/countOccurence.js
@@ -10,3 +10,4 @@ function countOccurence(arr) {
 }
 
 console.log(countOccurence([1, 2, 3, 2, 3, 4, 5])); // Output: { 1: 1, 2: 2, 3: 2, 4: 1, 5: 1 }
+console.log(countOccurence([1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5])); // Output: { 1: 1, 2: 2, 3: 2, 4:
