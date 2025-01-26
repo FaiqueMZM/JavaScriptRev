fcc9b52f91f6a75ab4490bb808254c79789ca0e8
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-26 17:02:16 +0530

    updated common key from two objects

--- DataStructureAndAlgorithm/Objects/commonKey.js
+++ DataStructureAndAlgorithm/Objects/commonKey.js
@@ -5,3 +5,11 @@ function commonKey(obj1, obj2) {
 }
 
 console.log(commonKey({ a: 1, b: 2, c: 3 }, { c: 4, d: 5, e: 6 })); // Output: ['c']
+
+// using built-in methods
+
+function commonKey(obj1, obj2) {
+    return Object.keys(obj1).filter(key => obj2.hasOwnProperty(key));
+}
+
+console.log(commonKey({ a: 1, b: 2 }, { b: 4, c: 5, d: 6 })); // Output: ['b']
