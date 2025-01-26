661b1766662ff43651a4be47309d704e41f0920d
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-01-26 16:47:56 +0530

    updated object to array

--- DataStructureAndAlgorithm/Objects/convertObjectToArray.js
+++ DataStructureAndAlgorithm/Objects/convertObjectToArray.js
@@ -5,3 +5,11 @@ function obejctToArray(arr) {
 }
 
 console.log(obejctToArray({ a: 1, b: 2, c: 3 })); // Output: [ ['a', 1], ['b', 2], ['c', 3] ]
+
+// using built-in methods
+
+function objectToArray(obj) {
+    return Object.keys(obj).map(key => [key, obj[key]]);
+}
+
+console.log(objectToArray({ a: 1, b: 2, c: 3 })); // Output: [ ['a', 1], ['b', 2], ['c', 3] ]
