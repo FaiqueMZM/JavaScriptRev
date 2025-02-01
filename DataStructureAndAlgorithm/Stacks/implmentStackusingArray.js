c8ae35e7332ebb8127854a480c18628e113374d4
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-01 22:10:55 +0530

    added impllment stack using array

--- DataStructureAndAlgorithm/Stacks/implmentStackusingArray.js
+++ DataStructureAndAlgorithm/Stacks/implmentStackusingArray.js
@@ -28,9 +28,9 @@ class Stack {
   }
   
   let stack = new Stack();
-  stack.push(10);
-  stack.push(20);
-  stack.push(30);
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
   console.log(stack.pop()); 
   console.log(stack.peek()); 
   console.log(stack.size()); 
