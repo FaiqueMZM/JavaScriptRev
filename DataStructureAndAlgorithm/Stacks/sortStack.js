8be4361d3936fcb9e1f883b11e01be1635a5a34b
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-03 22:40:53 +0530

    added sort a stack

---
+++ DataStructureAndAlgorithm/Stacks/sortStack.js
@@ -0,0 +1,19 @@
+// sort a stack
+
+function sortStack(stack) {
+    let tempStack = [];
+
+    while (stack.length > 0) {
+        let temp = stack.pop();
+
+        while (tempStack.length > 0 && tempStack[tempStack.length - 1] > temp) {
+            stack.push(tempStack.pop());
+        }
+
+        tempStack.push(temp);
+    }
+
+    return tempStack;
+}
+
+console.log(sortStack([34, 3, 31, 98, 92, 23])); // Output: [3, 23, 31, 34, 92, 98]
